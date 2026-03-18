eeb328b746b7452ca44ca1406e53728ce3356a8c
Hyuntae Jeong <jhtae0809@gmail.com>
2026-03-18 02:50:42 +0000

    sudokuing

--- homework3_hzj5293.py
+++ homework3_hzj5293.py
@@ -25,6 +25,7 @@ class DominoesGame(object):
         self.__board = board
         self.__m = len(board)
         self.__n = len(board[0])
+        self.inf = 10**100
 
     def get_board(self):
         return self.__board
@@ -72,7 +73,22 @@ class DominoesGame(object):
 
     # Required
     def get_best_move(self, limit, vertical):
-        pass
+        self.leaf_count = 0
+        score, move = self.helper(limit, vertical, -self.inf, self.inf, 0)
+        return score, move, self.leaf_count
+
+
+    def helper(self, limit, vertical, alpha, beta, curr):
+        
+        for move, b in self.successors(vertical):
+            if limit != 0:
+                new_result = (move, b.legal_moves(vertical)-b.legal_moves(not vertical), n_leaf)
+                new_alpha = alpha
+                if new_alpha < new_result[1]:
+                    new_alpha = new_result[1]
+                if b.legal_moves(vertical) - b.legal_moves(not vertical) > result[1]:
+                    result = new_result
+
 
 
 ############################################################
@@ -80,24 +96,54 @@ class DominoesGame(object):
 ############################################################
 
 def sudoku_cells():
-    pass
+    return [(i//9, i%9) for i in range(81)]
 
 def sudoku_arcs():
-    pass
+    cells = sudoku_cells()
+    arcs = []
+    for i in range(9):
+        block = [(i, j) for j in range(9)]
+        for j in block:
+            for k in block:
+                arcs.append((j,k))
+                arcs.append((k,j))
+        block = [(j, i) for j in range(9)]
+        for j in block:
+            for k in block:
+                arcs.append((j,k))
+                arcs.append((k,j))
+    for i in range(3):
+        for j in range(3):
+            block = [(i*3+k//3, j*3+k%3) for k in range(9)]
+            for ii in block:
+                for jj in block:
+                    if not (ii, jj) in arcs:
+                        arcs.append(ii,jj)
+        
+        
 
 def read_board(path):
-    pass
-
+    with open(path, 'r', encoding='utf-8') as f:
+        text = f.read().split()
+        d={}
+        for i in range(9):
+            for j in range(9):
+                if text[i][j] == "*":
+                    d[(i,j)]=set(range(1,10))
+                else:
+                    d[(i,j)] = text[i][j]
+        return d
+#print(read_board("sudoku/sudoku/hw3-easy.txt"))
 class Sudoku(object):
 
     CELLS = sudoku_cells()
     ARCS = sudoku_arcs()
 
     def __init__(self, board):
-        pass
+        self.__board= board
 
     def get_values(self, cell):
-        pass
+        return self.__board[cell]
 
     def remove_inconsistent_values(self, cell1, cell2):
         pass
